6f57e21bbcb5bc77fc051079da1929e091c14c4d
Samon03 <samon3123@gmail.com>
2020-10-20 22:16:50 +0600

    .env

--- AngularJS/src/app/employee/employee.component.ts
+++ AngularJS/src/app/employee/employee.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { log } from 'console';
 import { Employee } from '../shared/employee.model';
 
 import { EmployeeService } from '../shared/employee.service';
--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,17 +1,45 @@
+const path = require('path');
+const fs = require('fs');
+
+const compression = require('compression');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
-const { mongoose } = require('./db.js');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
 
 var employeeController = require('./controllers/employee.js');
 
-var app = express();
+dotenv.config();
+
+mongoose.connect(
+`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.n1dim.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`, (err) => {
+    if (!err) {
+        console.log('===== Connected!! =====');
+    } else {
+        console.log('===== Error!! =====' + JSON.stringify(err, undefined, 2));
+    }
+});
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'),
+    { flags: 'a' }
+  );
+
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.json());
 
 app.use(cors({ origin: 'http://localhost:4200' }));
 
-app.listen(3000, () => console.log('Server started at port : 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started at port : 3000'));
 
 app.use('/employees', employeeController);
